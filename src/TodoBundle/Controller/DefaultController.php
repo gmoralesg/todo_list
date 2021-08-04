274712a4ba4d31a8cb2cb787b7e5ac1e06f4dcb4
Andrés Morales <andres_parla92@hotmail.com>
2021-08-04 20:02:49 +0200

    poder añadir elemetos y visualizar

--- src/TodoBundle/Controller/DefaultController.php
+++ src/TodoBundle/Controller/DefaultController.php
@@ -2,15 +2,20 @@
 
 namespace TodoBundle\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use TodoBundle\Entity\Todo;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/list", name="todo_list")
+     * @Method("GET")
      */
-    public function indexAction()
+    public function listAction()
     {
         $list = $this->getDoctrine()->getRepository('TodoBundle:Todo')->findAll();
         // replace this example code with whatever you need
@@ -18,5 +23,26 @@ class DefaultController extends Controller
             'result' => $list
         ]);
        
+    }
+
+    /**
+     * @Route("/insert", name="todo_insert")
+     * @Method("POST")
+     */
+    public function insertAction(Request $request)
+    {
+       
+
+        $new = new Todo();
+        $new->setNombre($request->get("nombre"));
+        $new->setFechaCreacion( new DateTime(date('Y-m-d H:i:s')));
+        $new->setFechaTope( new DateTime(date('Y-m-d H:i:s',strtotime($request->get("fecha_tope")))));
+        $new->setEstado(false);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($new);
+        $em->flush();
+
+        return $this->redirectToRoute('todo_list');
     }
 }
